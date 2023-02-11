d8620b3733efc60e3cd33b41e8656036de0cc456
redflag-HSH <66863292+redflag-HSH@users.noreply.github.com>
2023-02-11 15:56:23 +0900

    rod test End

--- Assets/00.Scripts/Fising/FishingRod.cs
+++ Assets/00.Scripts/Fising/FishingRod.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class FishingRod : MonoBehaviour
 {
-    [SerializeField] float mSpeed;
+    [SerializeField] float mSpeed, currentVelocity, MaxVelocity; [Space]
+    [SerializeField] float LimitLength;
     Rigidbody2D rigid;
     // Start is called before the first frame update
     void Start()
@@ -15,14 +17,53 @@ public class FishingRod : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        rigid.velocity = rigid.velocity * 0.99f;
 
+        currentVelocity = rigid.velocity.x;
+
+        CheckLimit();
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            MoveHorizontal(false);
+        if (Input.GetKey(KeyCode.RightArrow))
+            MoveHorizontal(true);
     }
     public void MoveHorizontal(bool isRight)
     {
+        print("move horizontal");
+
+        int I = 0;
         if (isRight)
-            rigid.AddForce(new Vector2(mSpeed, 0));
+            I = 1;
         else
-            rigid.AddForce(new Vector2(-mSpeed, 0));
+            I = -1;
+
+
+        if (Mathf.Abs(currentVelocity) < MaxVelocity)
+        {
+            if (I == 1)
+                currentVelocity += mSpeed;
+            else
+                currentVelocity -= mSpeed;
+        }
+        else
+            currentVelocity = I * MaxVelocity;
+
+        rigid.velocity = new Vector2(currentVelocity, 0);
+
+    }
+    public void CheckLimit()
+    {
+        if (transform.position.x >= LimitLength)
+        {
+            rigid.velocity = Vector2.zero;
+            transform.position = new Vector2(LimitLength, transform.position.y);
+        }
+        else if (transform.position.x <= -LimitLength)
+        {
+            rigid.velocity = Vector2.zero;
+            transform.position = new Vector2(-LimitLength, transform.position.y);
+        }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
