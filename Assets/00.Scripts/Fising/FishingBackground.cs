d4744e788577790ee43e9642691e7a5cbd7b6f60
redflag-HSH <66863292+redflag-HSH@users.noreply.github.com>
2023-02-11 16:33:29 +0900

    generate WIP

--- Assets/00.Scripts/Fising/FishingBackground.cs
+++ Assets/00.Scripts/Fising/FishingBackground.cs
@@ -8,7 +8,7 @@ public class FishingBackground : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-        GetComponent<FishingObstacleGenerate>().Generate();
+        //GetComponent<FishingObstacleGenerate>().Generate();
 
     }
     // Update is called once per frame
--- Assets/00.Scripts/Fising/FishingObstacleGenerate.cs
+++ Assets/00.Scripts/Fising/FishingObstacleGenerate.cs
@@ -9,8 +9,12 @@ public class FishingObstacleGenerate : MonoBehaviour
     {
 
     }
-    public void Generate()
+    public void Generate(float length, float height, int count)
     {
-
+        for (int i = 0; i < count; i++)
+        {
+            int f = Random.Range(0, preset.Count);
+            //Instantiate(preset[f],new Vector2(Random.Range(-length,length),Random.Range(h)), transform.rotation, this.transform);
+        }
     }
 }
