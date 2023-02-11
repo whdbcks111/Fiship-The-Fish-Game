5350a4ddc7b043e1ca3229a43f89d35f37d306d4
whdbcks111 <81742754+whdbcks111@users.noreply.github.com>
2023-02-11 16:52:47 +0900

    game over scene generated

--- Assets/00.Scripts/Global/GameManager.cs
+++ Assets/00.Scripts/Global/GameManager.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using System;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
     public int Score = 0;
+    public float RodDurability = 100;
     
     [SerializeField] private TextMeshProUGUI _scoreText;
 
+    public void ResetGame() {
+        RodDurability = 100;
+    }
 
     private void Awake() {
         Instance = this;
@@ -21,5 +26,10 @@ public class GameManager : MonoBehaviour
     private void Update() {
         if(!(_scoreText is null)) 
             _scoreText.SetText(String.Format("Score : {0:00000000}", Score));
+
+        if(RodDurability <= 0) {
+            GameManager.Instance.ResetGame();
+            SceneManager.LoadScene("GameOverScene");
+        }
     }
 }
