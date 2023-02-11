c2d1136b9656e7dcfa7aaafb102d84accf3e3043
whdbcks111 <81742754+whdbcks111@users.noreply.github.com>
2023-02-11 17:18:00 +0900

    durability progress bar generated

--- Assets/00.Scripts/Global/GameManager.cs
+++ Assets/00.Scripts/Global/GameManager.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 using TMPro;
 using System;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
     public int Score = 0;
+    public float MaxRodDurability = 100;
     public float RodDurability = 100;
     
     [SerializeField] private TextMeshProUGUI _scoreText;
+    [SerializeField] private TextMeshProUGUI _durabilityText;
+    [SerializeField] private Image _durabilityProgress;
 
     public void ResetGame() {
-        RodDurability = 100;
+        RodDurability = MaxRodDurability;
     }
 
     private void Awake() {
@@ -25,7 +29,21 @@ public class GameManager : MonoBehaviour
 
     private void Update() {
         if(!(_scoreText is null)) 
-            _scoreText.SetText(String.Format("Score : {0:00000000}", Score));
+            _scoreText.SetText(String.Format("점수 : {0:00000000}", Score));
+        if(!(_durabilityText is null)) {
+            var ratio = RodDurability / MaxRodDurability;
+            if(RodDurability < MaxRodDurability * 0.4f) {
+                _durabilityProgress.color = Color.Lerp(Color.red, Color.yellow, ratio / 0.4f);
+            }
+            else if(RodDurability < MaxRodDurability * 0.8f) {
+                _durabilityProgress.color = Color.Lerp(Color.yellow, Color.green, (ratio - 0.4f) / (0.8f - 0.4f));
+            }
+            else {
+                _durabilityProgress.color = Color.green;
+            }
+            _durabilityText.SetText(String.Format("{0:0}%", ratio * 100));
+            _durabilityProgress.fillAmount = ratio;
+        }
 
         if(RodDurability <= 0) {
             GameManager.Instance.ResetGame();
